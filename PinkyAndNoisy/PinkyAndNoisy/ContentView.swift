75e1632e027de8662062c780190dfe05a0f5ccbc
Denis Cherniy <d.chernyi@storytelling-software.com>
2021-01-16 13:45:37 +0300

    Created empty project.

---
+++ PinkyAndNoisy/PinkyAndNoisy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PinkyAndNoisy
+//
+//  Created by Denis Cherniy on 16.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PinkyAndNoisy/PinkyAndNoisy/PinkyAndNoisyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinkyAndNoisyApp.swift
+//  PinkyAndNoisy
+//
+//  Created by Denis Cherniy on 16.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct PinkyAndNoisyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
